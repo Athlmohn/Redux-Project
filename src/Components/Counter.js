386eaa44cdf2ff716c6018c65d636ee3136eb531
Athlmohn <athulmnair01@gmail.com>
2023-06-11 17:49:59 +0530

    Intial Commit

---
+++ src/Components/Counter.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Counter.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { increment,decrement } from '../Redux/Reducer/Reducer'
+function Counter() {
+    const count = useSelector((state)=>state.counter.value)
+    const dispatch = useDispatch()
+  return (
+    <div className='counter-container'>
+    <h1 className='counter-title'>React Redux Sample</h1>
+     
+     <p className='counter-value'>{count}</p>
+      
+      <div className='counter-buttons'>
+
+       <button className='counter-btn' onClick={()=>dispatch(increment
+        ())}>Increment</button>
+       <button className='counter-btn' onClick={()=>dispatch(decrement())}>Decrement</button>
+      </div>
+
+    </div>
+  )
+}
+
+export default Counter
